e2c05dfce37f2e4c3602f54e9d16afd65330d529
Krishna Prasad - CONTRACT <kprasad@tractorsupply.com>
2024-05-30 20:14:36 +0530

    Image upload

--- k-shop/src/App.js
+++ k-shop/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+import { Routes } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
+import Cart from './pages/Cart/Cart'
+import Home from './pages/Home/Home'
+import Placeorder from './pages/PlaceOrder/Placeorder'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/order' element={<Placeorder/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ k-shop/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ExploreMenu from '../../components/Exploremenu/ExploreMenu'
+import Header from '../../components/Header/Header'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        <ExploreMenu/>
+    </div>
+  )
+}
+
+export default Home
