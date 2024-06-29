097ee9500d82b30bc3da23226b65034087143286
Krishna Prasad - CONTRACT <kprasad@tractorsupply.com>
2024-06-29 15:14:06 +0530

    Format document

--- k-shop/src/pages/Cart/Cart.jsx
+++ k-shop/src/pages/Cart/Cart.jsx
@@ -43,28 +43,28 @@ const Cart = () => {
         <div className="cart-total">
           <h2>Cart Totals</h2>
           <div className='cart-total-details'>
-          <p>Subtotal</p>
-          <p>{0}</p>
-        </div>
-        <hr />
-        <div className='cart-total-details'>
-          <p>Delivery Fee</p>
-          <p>{2}</p>
-        </div>
-        <hr />
-        <div className='cart-total-details'>
-          <p>Total</p>
-          <p>{0}</p>
-        </div>
-        <button>PROCCED TO CHECKOUT</button>
+            <p>Subtotal</p>
+            <p>{0}</p>
+          </div>
+          <hr />
+          <div className='cart-total-details'>
+            <p>Delivery Fee</p>
+            <p>{2}</p>
+          </div>
+          <hr />
+          <div className='cart-total-details'>
+            <p>Total</p>
+            <p>{0}</p>
+          </div>
+          <button>PROCCED TO CHECKOUT</button>
         </div>
         <div className="cart-promocode">
           <div>
             <p>If you have promo code, Enter it here.</p>
-          <div className='cart-promocode-input'>
-<input type='text' placeholder='promo code'></input>
-          <button>Submit</button>
-          </div>
+            <div className='cart-promocode-input'>
+              <input type='text' placeholder='promo code'></input>
+              <button>Submit</button>
+            </div>
           </div>
         </div>
       </div>
