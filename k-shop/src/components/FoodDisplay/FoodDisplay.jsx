bed0dcd55e7a3ce58ae83ddb993d8cdfc9e8252b
Krishna Prasad - CONTRACT <krishnaprasath00@gmail.com>
2024-06-29 15:18:54 +0530

    Format code

--- k-shop/src/components/FoodDisplay/FoodDisplay.jsx
+++ k-shop/src/components/FoodDisplay/FoodDisplay.jsx
@@ -4,19 +4,19 @@ import { StoreContext } from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
 import './FoodDisplay.css'
 
-const FoodDisplay = ({category}) => {
+const FoodDisplay = ({ category }) => {
 
-    const {food_list} = useContext(StoreContext)
+  const { food_list } = useContext(StoreContext)
   return (
     <div className='food-display' id='food-display'>
-        <h2>Top dishes near you</h2>
-        <div className="food-display-list">
-            {food_list.map((item, index)=>{
-              if (category === 'All' || category === item?.category) {
-                return<FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
-              }
-              })}
-        </div>
+      <h2>Top dishes near you</h2>
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          if (category === 'All' || category === item?.category) {
+            return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+          }
+        })}
+      </div>
     </div>
   )
 }
