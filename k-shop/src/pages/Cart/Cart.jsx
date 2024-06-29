05647ec41b05ed95114fc54ab333e2a7bb2079c7
Krishna Prasad - CONTRACT <krishnaprasath00@gmail.com>
2024-06-29 15:15:54 +0530

    removed unwanted code

--- k-shop/src/pages/Cart/Cart.jsx
+++ k-shop/src/pages/Cart/Cart.jsx
@@ -39,7 +39,7 @@ const Cart = () => {
           }
         })}
       </div>
-      <div className='cart-bottom'>
+      <div className='cart-bottom'> 
         <div className="cart-total">
           <h2>Cart Totals</h2>
           <div className='cart-total-details'>
